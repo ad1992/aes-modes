bfc6fbe45f87088c7c72f879212e2edcbf417f58
Aakansha Doshi <aakansha1216@gmail.com>
2024-03-05 12:31:24 +0530

    feat: add encrypt/decrypt views

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import "./App.scss";
+import DecyrptView from "./DecryptView";
+import EncryptView from "./EncryptView";
+import clsx from "clsx";
+import GitHubCorner from "./GitHubCorner";
+const VIEW = {
+  ENCRYPT: 0,
+  DECRYPT: 1,
+};
+function App() {
+  const [activeView, setActiveView] = useState(VIEW.ENCRYPT);
+  return (
+    <div className="App">
+      <GitHubCorner />
+      <h1>AES MODES </h1>
+      <div>
+        <button
+          className={clsx("encrypt", { active: activeView === VIEW.ENCRYPT })}
+          onClick={() => setActiveView(VIEW.ENCRYPT)}
+        >
+          Encrypt
+        </button>
+        <button
+          className={clsx("decrypt", { active: activeView === VIEW.DECRYPT })}
+          onClick={() => setActiveView(VIEW.DECRYPT)}
+        >
+          Decrypt
+        </button>
+      </div>
+      {activeView === VIEW.ENCRYPT ? <EncryptView /> : <DecyrptView />}
+    </div>
+  );
+}
+
+export default App;
